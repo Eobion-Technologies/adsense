46e82be968626668496bf1cd7073173765a05476
Khalid Baheyeldin <kbahey@gmail.com>
2006-07-11 04:34:37 +0000

    - Added update for creating database tables for existing installation. - The js file now sends a ?u= parameter with the url of the clicked page - Added new column to store the path upon clicking. This is useful in   tracking what was clicked, when, and by whom. This is useful for you   to know which pages get the most clicks, as well as in click fraud   disputes - Added update for creating database tables for existing installations.

--- adsense_click.js
+++ adsense_click.js
@@ -1,6 +1,5 @@
 /*
  * adsense_click.js - fires counter to log adsense clicks
- * original concept taken from http://www.webmasterworld.com/forum89/1788.htm
  */
 var lastStatus = '';
 function aslog(e) {
@@ -8,7 +7,7 @@ function aslog(e) {
   if (window.status && (window.status!=lastStatus)) {
     lastStatus = window.status;
     var bug = new Image();
-    bug.src = window.location.protocol + '//' + window.location.host + '/adsense/counter';
+    bug.src = window.location.protocol + '//' + window.location.host + '/adsense/counter' + '?u=' + escape(document.location);
   }
 }
 
@@ -23,23 +22,4 @@ for (var i = 0; i < elements.length; i++) {
     elements[i].onfocus = aslog;
     iframeObj = elements[i];
   }
-}
-
-function getVariable(name) {
-  var dc = iframeObj.src;
-  var prefix = name + "=";
-  var begin = dc.indexOf("&" + prefix);
-  if (begin == -1) {
-    begin = dc.indexOf("?" + prefix);
-    if (begin == -1) {
-      return null;
-    }
-  } else {
-    begin += 1;
-  }
-  var end = iframeObj.src.indexOf("&", begin);
-  if (end == -1) {
-    end = dc.length;
-  }
-  return unescape(dc.substring(begin + prefix.length, end));
 }
