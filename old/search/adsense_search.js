f1114018e5b01ba86ac1a7ab64646f64563e2197
Joao Ventura <jcnventura-drupal@venturas.org>
2008-08-20 22:24:42 +0000

    Farbtastic working 100%

--- old/search/adsense_search.js
+++ old/search/adsense_search.js
@@ -2,21 +2,23 @@
 
 if (Drupal.jsEnabled) {
   $(document).ready(function () {
-    var colorFields = new Array();
-    colorFields[0] = "#edit-adsense-search-color-border";
-    colorFields[1] = "#edit-adsense-search-color-title";
-    colorFields[2] = "#edit-adsense-search-color-bg";
-    colorFields[3] = "#edit-adsense-search-color-text";
-    colorFields[4] = "#edit-adsense-search-color-url";
-    colorFields[5] = "#edit-adsense-search-color-visited-url";
-    colorFields[6] = "#edit-adsense-search-color-light-url";
-    colorFields[7] = "#edit-adsense-search-color-logo-bg";
-
     // initiate farbtastic colorpicker
     var farb = $.farbtastic("#colorpicker");
+    var firstField = "";
 
-    for (x in colorFields) {
-      farb.linkTo(colorFields[x]);
-    };
+    $("input.form-text").each( function() {
+      if (this.name.substring(0, 20) == "adsense_search_color") {
+        if (firstField == "") {
+          firstField = this;
+        };
+
+        farb.linkTo(this);
+        $(this).click(function () {
+          farb.linkTo(this);
+        });
+      };
+    });
+
+    farb.linkTo(firstField);
   });
 }
