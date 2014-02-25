00dfd2e97e24c734f7504fd142eb14727cced608
Joao Ventura <jcnventura-drupal@venturas.org>
2014-02-25 18:50:36 +0000

    Work around the current EasyList ad blocks.

--- js/unblock.js
+++ js/unblock.js
@@ -1,7 +1,7 @@
 (function ($) {
 Drupal.behaviors.adSenseUnblock = {
   attach: function (context) {
-    if ($('.adsense ins').height() === null) {
+    if ($('.adsense ins').contents().length == 0) {
       $('.adsense').html(Drupal.t("Please, enable ads on this site. By using ad-blocking software, you're depriving this site of revenue that is needed to keep it free and current. Thank you."));
       $('.adsense').css({'overflow': 'hidden', 'font-size': 'x-small'});
     }
