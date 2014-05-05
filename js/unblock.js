2af588194365ef6cbdb03599202e37aa7f06ce59
João Ventura <jcnventura@122464.no-reply.drupal.org>
2014-05-05 17:10:25 +0000

    Use slightly larger font in unblock message.

--- js/unblock.js
+++ js/unblock.js
@@ -4,7 +4,7 @@ Drupal.behaviors.adSenseUnblock = {
     setTimeout(function() {
       if ($('.adsense ins').contents().length == 0) {
         $('.adsense').html(Drupal.t("Please, enable ads on this site. By using ad-blocking software, you're depriving this site of revenue that is needed to keep it free and current. Thank you."));
-        $('.adsense').css({'overflow': 'hidden', 'font-size': 'x-small'});
+        $('.adsense').css({'overflow': 'hidden', 'font-size': 'smaller'});
       }
     }, 3000); // wait 3 seconds for adsense async to execute
   }
